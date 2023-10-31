34b105037e4db3cf2c04cde7ac6a8cf78c7cd5a9
krystiano13 <crystianotv@gmail.com>
2023-10-31 18:50:01 +0100

    implement interface to use program

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
-        Punk punk = new Punk(true);
-        punk.setEndpoint();
-        punk.getRequest();
+
+        System.out.println("Write a number or 'random' : ");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+
+        System.out.println("Proccessing ...");
+
+        if(name.equals("random")) {
+            Punk punk = new Punk(true);
+            punk.setEndpoint();
+            punk.getRequest();
+        }
+
+        else {
+            try {
+                Punk punk = new Punk(false);
+                punk.setBeerId(Integer.parseInt(name));
+                punk.setEndpoint();
+                punk.getRequest();
+            }
+            catch(Exception e) {
+                System.out.println("Error");
+            }
+        }
     }
 }
